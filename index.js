3b1d94cde2ff2878a026c0ae0b1abf7dd98fbd2c
Sara Hopper <sara.hopper@gmail.com>
2017-09-26 17:10:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.upcase + '!';
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
